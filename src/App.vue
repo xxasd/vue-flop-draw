9a7b3c04ced1863ace387c522d0c5cb737f56374
sungaosheng <515798267@qq.com>
2021-09-14 18:14:04 +0800

    npm build

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
-import FlowDraw from "./packages/FlowDraw.vue";
+import FlowDraw from "./packages/flow-draw/src/flow-draw.vue";
 
 export default defineComponent({
   name: "App",
